b6041bccdff99eff3561237c93146b064090a774
RabiaAtif329 <160391973+RabiaAtif329@users.noreply.github.com>
2024-03-05 23:34:04 +0500

    Add files via upload

---
+++ name_case.ts
@@ -0,0 +1,4 @@
+let personname = "Rabia Atif";
+console.log(personname.toLowerCase());
+console.log(personname.toLocaleUpperCase());
+console.log(personname.replace(/\b\w/g, (char) => char.toUpperCase()));
