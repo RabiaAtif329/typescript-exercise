50cc3a7a2e2e90ee2b03a824c309e1b45c6e7ec6
RabiaAtif329 <160391973+RabiaAtif329@users.noreply.github.com>
2024-03-06 20:23:20 +0500

    Add files via upload

---
+++ 21.typescript_object.ts
@@ -0,0 +1,13 @@
+interface itcourse {
+    coursename: string;
+    location:string;
+    onsitestudents : number;
+
+}
+
+let itcourse = {
+    coursename : "typescript and javascripy",
+    location : "governor house sindh",
+    onsitestudents : 5000 
+};
+console.log(itcourse);
