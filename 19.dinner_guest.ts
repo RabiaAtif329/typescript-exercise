65be121498028c77450904935e58194314a86d33
RabiaAtif329 <160391973+RabiaAtif329@users.noreply.github.com>
2024-03-06 07:04:55 +0500

    Add files via upload

---
+++ 19.dinner_guest.ts
@@ -0,0 +1,10 @@
+let guestList = ["Abeeha","Beenish","Mehwish","Atif","Umer","Aisha"];
+
+//guestlist.forEach(oneguest => console.log(`Salam ${oneguest},would you like to dinner with me?`));
+
+let lengthGuests: number = guestList.length;
+
+console.log(`we are Inviting total ${lengthGuests}`);
+
+
+
