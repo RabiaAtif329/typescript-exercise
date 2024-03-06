545247f7ac11c113a581c05ccf0760f99c2b3165
RabiaAtif329 <160391973+RabiaAtif329@users.noreply.github.com>
2024-03-06 07:06:24 +0500

    Add files via upload

---
+++ 20.store in array.ts
@@ -0,0 +1,8 @@
+//making of a programming languages array
+let programminglanguages: string[] = ["javascript","typescript","python","html"];
+
+//print the message of list
+console.log("list of programming languages:");
+
+//print the values of array in a form of 
+programminglanguages.forEach(language => console.log(language));
