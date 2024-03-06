6f25bca15d1dca1d380327092bb11cc69e37ca61
RabiaAtif329 <160391973+RabiaAtif329@users.noreply.github.com>
2024-03-06 20:24:40 +0500

    Add files via upload

---
+++ 22.intentional_error.ts
@@ -0,0 +1,8 @@
+// Creating a Array
+let errorarray: string[] = ["A","B","C","D"];
+
+// Producing error ! by Accessing invalid index of array
+//console.log(errorarray[10]);
+
+//error removed
+console.log(errorarray[2]);
