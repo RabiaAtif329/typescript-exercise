131817fbe2075dfbc96ccc3349134521e7dfa9fc
RabiaAtif329 <160391973+RabiaAtif329@users.noreply.github.com>
2024-03-06 20:27:29 +0500

    Add files via upload

---
+++ 23.conditional_test.ts
@@ -0,0 +1,35 @@
+//making a variable 
+let five = 5;
+
+let ten = 10;
+
+//test 1
+console.log("test 1: five is equal to 5?");
+console.log(five == 5);
+//test 2
+console.log("\ntest 2:ten is equal to 10?");
+console.log(ten == 10);
+//test 3
+console.log("\ntest 3:five is not equal to ten?")
+console.log(five != 10);
+//test 4 
+console.log("\ntest 4 :10 is greater than 5?");
+console.log(ten > 5 );
+//test 5 
+console.log("test 5:five is smaller than ten?");
+console.log(five < 10);
+//test 6
+console.log("test 6:5 is equal to 10");
+console.log(five == 10);
+//test 7
+console.log("test 7:10 is smaller than 5?");
+console.log (10 < 5);
+//test 8
+console.log("test 8:5 is greater than 10?");
+console.log(5 > 10);
+//test 9
+console.log("test 9:10 is not equal to 10?");
+console.log(10 != 10);
+//test 10
+console.log("test 10:10 is greater than 10?");
+console.log(10 > 10);
