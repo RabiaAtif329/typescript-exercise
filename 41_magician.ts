e9c2aff637967729887b6b567c5532b25f00207e
RabiaAtif329 <160391973+RabiaAtif329@users.noreply.github.com>
2024-03-17 15:10:58 +0500

    Add files via upload

---
+++ 41_magician.ts
@@ -0,0 +1,14 @@
+// Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(),
+//which prints the name of each magician in the array.
+
+let magician:string[] = ['herry potter','qasim','asim','shreya'];
+function show_magicians(magiciansName:string[]){
+magiciansName.forEach(element => {
+    console.log(element);
+        
+    });
+        
+    }
+
+
+
